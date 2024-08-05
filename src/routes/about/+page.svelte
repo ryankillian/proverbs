0ea2b7858c3c679bbe5d81caec8ccc31b42d20d3
killian ryan <killian_ryan@hotmail.com>
2024-08-05 14:48:15 +0100

    feat: Add About

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,3 +1,26 @@
-<h1>Hi</h1>
+<svelte:head>
+	<title>About - Proverbs Project</title>
+	<meta name="description" content="Information page for the Proverbs Project website" />
+</svelte:head>
 
-<p>This is my about page.</p>
+<h1>About This Site</h1>
+
+<article>
+	<p>
+		This site is a small personal project aimed at staying up-to-date with modern full-stack web
+		development and exploring the use of language models (LLMs) for analysis.
+	</p>
+</article>
+
+<article>
+	<p>
+		Proverbs featured here are sourced from Morris Palmer Tilley's
+		<a href="https://hdl.handle.net/2027/mdp.39015016495585" target="_blank"
+			>"A Dictionary of the Proverbs in England in the Sixteenth and Seventeenth Centuries"</a
+		>
+		licensed under a
+		<a href="http://creativecommons.org/licenses/by-nc/3.0/" target="_blank"
+			>Creative Commons Attribution-NonCommercial 3.0 Unported License</a
+		>.
+	</p>
+</article>
